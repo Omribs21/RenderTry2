dc7fcf22de1d3a5519a2b34b91278de34d88094a
omri <omr2361@gmail.com>
2022-12-03 16:19:27 +0200

    8

--- backend/settings.py
+++ backend/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'base.apps.BaseConfig',
-    'rest_framework_simplejwt', #dont work here
+    #'rest_framework_simplejwt', #dont work here
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     "corsheaders",
